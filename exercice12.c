d698a31f9ab8e216df3d15592c2aeb2df3b67c7c
CyrilleMarc <cyrillemarc1@gmail.com>
2023-10-07 11:48:11 +0200

    tableau

--- exercice12.c
+++ exercice12.c
@@ -1,14 +1,24 @@
 #include <stdio.h>
 
+#define TAILLE_MAX 10
+int t[TAILLE_MAX] = {0, 12, 345, 56, 61, 99, 0, 11, 45, 100};
+
 int main()
 {
+    int *pstart = NULL;
+    int *pend = NULL;
+    int *pNumber = NULL;
 
-    int t[100] = {0, 2, 6, 67, 98, 0, 456, 67};
-    int size = sizeof(t[100] / sizeof(int));
+    pstart = &t[0];
+    pend = &t[TAILLE_MAX - 1];
 
-    for (int i = 0; i < size; ++i)
+    for (pNumber = pstart; pNumber <= pend; ++pNumber)
     {
-        t[i];
-        printf("index %d = valeur %d\n", i, t[i]);
+        if (*pNumber == 0)
+        {
+            printf("index = %ld et valeur = %d\n", (long)(pNumber - pstart), *pNumber);
+        }
     }
+
+    return 0;
 }
